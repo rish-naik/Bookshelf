baf787c99cda75453636bbcc2ab41fb1127479bd
Rish Naik <rish.naik06@gmail.com>
2026-05-18 20:33:13 -0500

    removed the explore anchor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,6 @@ function NavBar({ onSearch, openFavorites }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Explore
-        </a>
         <a onClick={() => openFavorites()} className="navbar-brand" href="#">
           My Bookshelf
         </a>
